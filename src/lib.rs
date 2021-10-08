8a55296f67e8f103c43769ff2a2cf5fb4e35d184
Cyborus04 <87248184+Cyborus04@users.noreply.github.com>
2021-10-08 15:39:37 -0400

    Add safety comments

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,10 @@ impl<const C: usize> InlineAny<C> {
             "alignment of type too large for `InlineAny`"
         );
         let type_id = TypeId::of::<T>();
+        // Safety: Still in question, see issue #2
         let drop_in_place = unsafe { transmute(std::ptr::drop_in_place::<T> as unsafe fn(*mut T)) };
         Self {
+            // Safety: assertions above, and the `Any` bound requires `'static`
             data: unsafe { TypeErased::new_unchecked(value) },
             type_id,
             drop_in_place,
@@ -54,6 +56,8 @@ impl<const C: usize> InlineAny<C> {
     /// Returns some reference to the boxed value if it is of type T, or None if it isn’t.
     pub fn downcast_ref<T: Any>(&self) -> Option<&T> {
         if self.is::<T>() {
+            // Safety:
+            // data is guaranteed to be of type `T`, there's no way to construct it where it isn't
             Some(unsafe { self.data.assume_type_ref() })
         } else {
             None
@@ -63,6 +67,8 @@ impl<const C: usize> InlineAny<C> {
     /// Returns some mutable reference to the boxed value if it is of type T, or None if it isn’t.
     pub fn downcast_mut<T: Any>(&mut self) -> Option<&mut T> {
         if self.is::<T>() {
+            // Safety:
+            // data is guaranteed to be of type `T`, there's no way to construct it where it isn't
             Some(unsafe { self.data.assume_type_mut() })
         } else {
             None
@@ -72,6 +78,13 @@ impl<const C: usize> InlineAny<C> {
     /// Attempt to downcast the box to a concrete type.
     pub fn downcast<T: Any>(self) -> Result<T, Self> {
         if self.is::<T>() {
+            // Safety:
+            // data is guaranteed to be of type `T`,
+            // there's no way to construct it where it isn't
+            //
+            // It's okay to have 2 copies of the data, since the raw bytes aren't interpreted as `T`
+            // so it's really just this one copy of it.
+            // As long as `self` is forgotten and `self.drop_in_place` isn't called, everything's fine
             let x = unsafe { self.data.as_ptr::<T>().read() };
             std::mem::forget(self);
             Ok(x)
@@ -83,6 +96,7 @@ impl<const C: usize> InlineAny<C> {
 
 impl<const C: usize> Drop for InlineAny<C> {
     fn drop(&mut self) {
+        // Safety: Still in question, see issue #2
         unsafe { (self.drop_in_place)(self.data.as_mut_ptr::<()>()) }
     }
 }
